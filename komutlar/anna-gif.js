0cf8f6289a7734072e23a3084da6e5a3fef395c8
devhill535 <77030591+devhill535@users.noreply.github.com>
2021-03-12 00:36:12 +0300

    Update anna-gif.js

--- komutlar/anna-gif.js
+++ komutlar/anna-gif.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-let replies = ["https://www.bing.com/th/id/OGC.aa383597be33543e833f67128cf31646?pid=1.7&rurl=http%3a%2f%2f3.bp.blogspot.com%2f-7HV6jjwgDr0%2fU74I5DxMU9I%2fAAAAAAAABEY%2fzajXCxGS4_g%2fs1600%2fpost-41000-Frozen-Anna-smiling-gif-Imgur-WEqF.gif&ehk=Pv2V4OJMkZdkDmIREV0m3XguE%2bJej0%2blOin4akl7pcA%3d","https://www.bing.com/th/id/OGC.a540af2fabd0fcc8f79d21b882f1f3c3?pid=1.7&rurl=https%3a%2f%2fgifimage.net%2fwp-content%2fuploads%2f2017%2f09%2fanna-frozen-gif-2.gif&ehk=MBXbNQg093dxKc6NbSd16O3j43kzlXyvQzSqd%2bmClXs%3d",""];
+let replies = ["https://www.bing.com/th/id/OGC.aa383597be33543e833f67128cf31646?pid=1.7&rurl=http%3a%2f%2f3.bp.blogspot.com%2f-7HV6jjwgDr0%2fU74I5DxMU9I%2fAAAAAAAABEY%2fzajXCxGS4_g%2fs1600%2fpost-41000-Frozen-Anna-smiling-gif-Imgur-WEqF.gif&ehk=Pv2V4OJMkZdkDmIREV0m3XguE%2bJej0%2blOin4akl7pcA%3d","https://www.bing.com/th/id/OGC.a540af2fabd0fcc8f79d21b882f1f3c3?pid=1.7&rurl=https%3a%2f%2fgifimage.net%2fwp-content%2fuploads%2f2017%2f09%2fanna-frozen-gif-2.gif&ehk=MBXbNQg093dxKc6NbSd16O3j43kzlXyvQzSqd%2bmClXs%3d","https://giffiles.alphacoders.com/959/95935.gif","https://giffiles.alphacoders.com/958/95851.gif","https://giffiles.alphacoders.com/956/95673.gif","https://giffiles.alphacoders.com/957/95730.gif","https://giffiles.alphacoders.com/957/95759.gif","https://giffiles.alphacoders.com/958/95836.gif","https://giffiles.alphacoders.com/166/166476.gif","https://giffiles.alphacoders.com/959/95946.gif","https://giffiles.alphacoders.com/956/95668.gif",""];
 let result = Math.floor((Math.random() * replies.length));
 
 let gifembed = new Discord.MessageEmbed()
