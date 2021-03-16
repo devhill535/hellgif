215295e989c7a36c0207e95f49273ce70b9e354d
devhill535 <77030591+devhill535@users.noreply.github.com>
2021-03-16 11:10:59 +0300

    Update anna-gif.js

--- komutlar/anna-gif.js
+++ komutlar/anna-gif.js
@@ -2,7 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-let replies = ["","","",];
+let replies = ["",
+               "",
+               "",
+               "",
+               "",
+              ];
 let result = Math.floor((Math.random() * replies.length));
 
 let gifembed = new Discord.MessageEmbed()
